25bcba16fd1c06a1600dd44089a00f29fe7441d4
SvetlanaVerzhbitskaya <svetlana.verzhbitskaya@gmail.com>
2019-10-14 11:19:18 +0300

    Личный прект: подробности

--- js/main.js
+++ js/main.js
@@ -13,11 +13,21 @@ var MIN_X = 100;
 var MAX_X = 660;
 var MIN_Y = 130;
 var MAX_Y = 630;
+var ENTER_KEYCODE = 13;
+var PIN_WIDTH_INACTIVE = 65;
+var PIN_HEIGHT_INACTIVE = 65;
+var PIN_TRIANGLE_HEIGHT = 22;
+var PIN_LEFT = 570;
+var PIN_TOP = 375;
 var similarPinElement = document.querySelector('.map__pins');
 var similarPinTemplate = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
 var fragment = document.createDocumentFragment();
+var setupForm = document.querySelector('.ad-form').querySelectorAll('fieldset');
+var setupFilters = document.querySelector('.map__filters').querySelectorAll('fieldset');
+var setupActive = document.querySelector('.map__pin--main');
+var setup = document.querySelector('.map');
 
 var getRandomNumber = function (arr) {
   numbers = arr[Math.floor(Math.random() * arr.length)];
@@ -69,11 +79,8 @@ var getNeighbours = function (arr) {
 
 getNeighbours(neighbours);
 
-document.querySelector('.map').classList.remove('map--faded');
-
 var renderPin = function (pin) {
   var pinElement = similarPinTemplate.cloneNode(true);
-
 
   pinElement.style = 'left: ' + pin.location.x + 'px; top: ' + pin.location.y + 'px';
   pinElement.querySelector('img').setAttribute('src', pin.author.avatar);
@@ -90,3 +97,77 @@ var createPin = function (arr) {
 };
 
 createPin(neighbours);
+
+
+var address = document.querySelector('input[id="address"]');
+var writeAddress = function (x, y) {
+  address.setAttribute('value', x + ', ' + y);
+}
+var addAddressInactive = function (left, top, width, height) {
+  writeAddress(Math.round(left + width / 2), Math.round(top + height / 2));
+};
+
+var addAddressActive = function (left, top, width, height, triangleHeight) {
+  writeAddress(Math.round(left + width / 2), Math.round(top + height + triangleHeight));
+};
+
+addAddressInactive(PIN_LEFT, PIN_TOP, PIN_WIDTH_INACTIVE, PIN_HEIGHT_INACTIVE);
+
+var makeFormDisabled = function (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    arr[i].setAttribute('disabled', true);
+  }
+};
+
+var makeFormActive = function (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    arr[i].removeAttribute('disabled');
+  }
+}
+
+makeFormDisabled(setupForm);
+makeFormDisabled(setupFilters);
+
+var activateMap = function () {
+  setup.classList.remove('map--faded');
+  makeFormActive(setupForm);
+  makeFormActive(setupFilters);
+  addAddressActive(PIN_LEFT, PIN_TOP, PIN_WIDTH_INACTIVE, PIN_HEIGHT_INACTIVE, PIN_TRIANGLE_HEIGHT);
+};
+
+setupActive.addEventListener('click', function() {
+  activateMap();
+});
+
+setupActive.addEventListener('keydown', function() {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    activateMap();
+  }
+});
+
+var rooms = document.querySelector('select[name="rooms"]');
+var roomsAmount;
+rooms.addEventListener('click', function() {
+  var roomsNumber = document.querySelector('select[name="rooms"]').selectedIndex;
+  roomsAmount = document.querySelector('select[name="rooms"]').options[roomsNumber].text;
+  console.log(roomsAmount);
+  return roomsAmount;
+});
+
+var guests = document.querySelector('select[name="capacity"]');
+guests.addEventListener('click', function() {
+  var guestsNumber = document.querySelector('select[name="capacity"]').selectedIndex;
+  var guestsAmount = document.querySelector('select[name="capacity"]').options[guestsNumber].text;
+
+  if ((roomsAmount === '1 комната' && guestsAmount === 'для 2 гостей') || (roomsAmount === '1 комната' && guestsAmount === 'для 3 гостей') || (roomsAmount === '1 комната' && guestsAmount === 'не для гостей')) {
+    guests.setCustomValidity('Возможно выбрать только для 1 гостя');
+  } else if ((roomsAmount === '2 комнаты' && guestsAmount === 'для 3 гостей') || (roomsAmount === '1 комната' && guestsAmount === 'не для гостей')) {
+    guests.setCustomValidity('Возможно выбрать только для 1 или 2 гостей');
+  } else if (roomsAmount === '3 комнаты' && guestsAmount === 'не для гостей') {
+    guests.setCustomValidity('Возможно выбрать только для 1, 2 или 3 гостей');
+  } else if ((roomsAmount === '100 комнат' && guestsAmount === 'для 1 гостя') || (roomsAmount === '100 комнат' && guestsAmount === 'для 2 гостей') || (roomsAmount === '100 комнат' && guestsAmount === 'для 3 гостей')) {
+    guests.setCustomValidity('Возможно выбрать только не для гостей');
+  } else {
+    guests.setCustomValidity('');
+  }
+});
